60a1a18a5acd0636d24d8d1d92e5fee15c05cfd8
ashirwad0201 <ashirwad0201@gmail.com>
2024-03-03 14:24:54 +0530

    sign up frontend and backend integration

---
+++ signup/sign-up.js
@@ -0,0 +1,33 @@
+async function onSignUp(e){
+    e.preventDefault();
+    var name_=document.getElementById('id1').value;
+    var email_=document.getElementById('id2').value;
+    var phone_=document.getElementById('id3').value;
+    var password_=document.getElementById('id4').value;
+
+    let myObj={
+        name: name_,
+        email: email_,
+        phone: phone_,
+        password: password_
+    }
+    if(name_!='' && email_!='' && phone_!='' && password_!=''){
+        try{
+            const result= await axios.get(`${API_ENDPOINT}admin/get-user/${email_}`);
+            if(result.data==""){
+               await axios.post(`${API_ENDPOINT}admin/insert-user`,myObj)
+               alert("Signed up successfully")
+            }
+            else{
+                alert('User already exists')
+            }
+
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
+    else{
+        alert('Please fill the empty fields!')
+    }
+}
